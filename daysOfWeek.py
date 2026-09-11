7f8ef0d27efca9163941054a6eb016022045f91a
mohitdhumal2005 <mohitdhumal2005@gmail.com>
2026-09-11 15:27:25 +0530

    .\evenOddCondi.py added

---
+++ daysOfWeek.py
@@ -0,0 +1,18 @@
+num=int(input("Enter a num: "))
+if(num==0):
+    print("0 is Sunday")
+elif(num==1):
+    print("monday")
+elif(num==2):
+    print("tuesday")
+elif(num==3):
+    print("wednesday")
+elif(num==4):
+    print("thursday")
+elif(num==5):
+    print("friday")
+elif(num==6):
+    print("saturday")
+else:
+    print("Bye bye")
+    

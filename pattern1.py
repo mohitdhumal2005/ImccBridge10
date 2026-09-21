3de5c107ad4eed724d3e0e7bd3d47817079d7f6f
mohitdhumal2005 <mohitdhumal2005@gmail.com>
2026-09-21 14:45:50 +0530

    two files added and committed

---
+++ pattern1.py
@@ -0,0 +1,15 @@
+'''         
+         *
+        ***
+       *****
+      *******
+'''
+
+for i in range(1,5):
+    for j in range(1,(4-i)+1):
+        print(" ",end="")
+        print
+    for k in range(1,(i*2-1)+1):
+        print("*",end="")
+    print()
+  
